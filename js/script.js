822b7ec396f1a72b60b8bd2173010a653e919538
sagits <renatopprobst@gmail.com>
2013-12-17 22:10:43 -0200

    js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-$(document).bind("mobileinit", function() {
-	$.mobile.defaultPageTransition = "none"
-});
-
 $(document).ready(function() {
 
 	$("#exit").click(function() {
 		navigator.app.exitApp();
 
 	});
+	
+	$(document).on("mobileinit", function(){
+    $.mobile.buttonMarkup.hoverDelay = 0;
+});
+
+$(document).on("mobileinit", function(){
+    $.mobile.defaultPageTransition   = 'none';
+    $.mobile.defaultDialogTransition = 'none';
+});
 
 });
